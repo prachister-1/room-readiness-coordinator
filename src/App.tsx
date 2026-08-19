abb9817f80f8fbcef7d8c0e2bc0cb9cb73f40f66
Ashish Bhatia <ashish@Ashishs-MacBook-Air.local>
2026-08-19 11:46:54 +0100

    Revert "Publish the TravelXen consultant prototype for a shareable demo."
    This reverts commit 7c9805dc0f759df62c03eb4d24da58a91c9944f0.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
 import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
-import { DemoProvider } from './consultant/store'
-import { ConsultantShell } from './consultant/Shell'
-import { IntakeQueue } from './consultant/IntakeQueue'
-import { ConsultantWorkspace } from './consultant/Workspace'
-import { QualityLearning } from './consultant/Learning'
+import { StoreProvider } from './state/Store'
+import { AppShell } from './components/layout/AppShell'
+import { ArrivalReadiness } from './pages/ArrivalReadiness'
+import { Housekeeping } from './pages/Housekeeping'
+import { Rooms } from './pages/Rooms'
+import { GuestMessages } from './pages/GuestMessages'
+import { Analytics } from './pages/Analytics'
+import { Settings } from './pages/Settings'
+import { AgentOrchestration } from './pages/AgentOrchestration'
+import { Playbooks } from './pages/Playbooks'
+import { Policies } from './pages/Policies'
+import { DecisionInbox } from './pages/DecisionInbox'
+import { ShiftHandover } from './pages/ShiftHandover'
 
 export default function App() {
   return (
-    <DemoProvider>
+    <StoreProvider>
       <HashRouter>
         <Routes>
-          <Route element={<ConsultantShell />}>
-            <Route path="/" element={<IntakeQueue />} />
-            <Route path="/workspace" element={<ConsultantWorkspace />} />
-            <Route path="/learning" element={<QualityLearning />} />
+          <Route element={<AppShell />}>
+            <Route path="/" element={<ArrivalReadiness />} />
+            <Route path="/inbox" element={<DecisionInbox />} />
+            <Route path="/orchestration" element={<AgentOrchestration />} />
+            <Route path="/playbooks" element={<Playbooks />} />
+            <Route path="/handover" element={<ShiftHandover />} />
+            <Route path="/policies" element={<Policies />} />
+            <Route path="/housekeeping" element={<Housekeeping />} />
+            <Route path="/rooms" element={<Rooms />} />
+            <Route path="/messages" element={<GuestMessages />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/settings" element={<Settings />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
         </Routes>
       </HashRouter>
-    </DemoProvider>
+    </StoreProvider>
   )
 }
